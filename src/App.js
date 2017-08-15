007d708c93f8da418c3c75d70c22fb851d80f0e7
Sasha Demyanik <ttsvan@msn.com>
2017-08-14 21:06:59 -0700

    Adds Books, Header and SearchInput components.
    Implements the basic Header and Books components and adds them to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import {Grid, Col, Row} from 'react-bootstrap';
+import Header from './Components/Header';
+import Books from './Components/Books';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books:[],
+      text:'Harry Potter'
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        A Work in progress
+        <Header />
+        <Grid>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              <Books />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
---
+++ src/Components/Books.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap';
+
+class Books extends Component {
+  render() {
+    return (
+      <div>
+        BOOKS
+      </div>
+    );
+  }
+}
+
+export default Books;
